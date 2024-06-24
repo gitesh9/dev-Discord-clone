8e5be56827a19b9a2cd152ad493bd60da53673d8
Gitesh <giteshwan98@gmail.com>
2024-06-24 14:39:08 +0530

    Invitations and Server settings

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { useEffect, useState } from "react";
+
+import { EditServerModal } from "@/components/modals/edit-server-modal";
+import { CreateServerModal } from "@/components/modals/create-server-modal";
+import { InviteModal } from "@/components/modals/invite-modal";
 
 export const ModalProvider = () => {
 	const [isMounted, setIsMouned] = useState(false);
@@ -17,6 +20,8 @@ export const ModalProvider = () => {
 	return (
 		<>
 			<CreateServerModal />
+			<InviteModal />
+			<EditServerModal />
 		</>
 	);
 };
